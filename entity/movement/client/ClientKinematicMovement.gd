c5222be2743de99a4f674f6715b333fb791f6418
fearlesspandas <landonrenzullo@gmail.com>
2023-11-14 02:16:21 -0500

    two way semi working

--- entity/movement/client/ClientKinematicMovement.gd
+++ entity/movement/client/ClientKinematicMovement.gd
@@ -2,13 +2,13 @@ extends Node
 
 class_name ClientKinematicMovement
 
-var speed = 100
+var speed = 50
 func move(delta,location:Vector3,body:KinematicBody):
-	print("moving:",location)
+	#print("moving:",location)
 	var diff:Vector3 = (body.global_transform.origin - location).normalized() * speed * delta
 	#body.move_and_collide(-diff,false)
 	body.move_and_slide_with_snap(-diff,Vector3.UP)
-	body.global_transform.origin -= diff
+	#body.global_transform.origin -= diff
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
--- entity/movement/server/ServerKinematicMovement.gd
+++ entity/movement/server/ServerKinematicMovement.gd
@@ -5,10 +5,11 @@ class_name ServerKinematicMovement
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var speed = 0.001
+var speed = 0.01
 func move(delta,location:Vector3,body:KinematicBody):
 	var diff:Vector3 = (body.global_transform.origin - location).normalized() * speed * delta
-	body.move_and_collide(diff,false)
+	#body.move_and_slide_with_snap(-diff,Vector3.UP)
+	body.global_transform.origin -= diff
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
