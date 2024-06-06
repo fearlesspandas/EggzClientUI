78798e1365f30d8803102abeee349e8c6f51aec3
fearlesspandas <landonrenzullo@gmail.com>
2024-06-06 15:53:37 -0400

    working bidirectional terrain

--- user_interface/TerrainScannerTimer.gd
+++ user_interface/TerrainScannerTimer.gd
@@ -12,8 +12,8 @@ func _ready():
 
 func poll_for_terrain():
 	if is_active:
-		var id = str(rand_range(-999,999))
+		var id = str(int(rand_range(-999,999)))
 		test_vec_delete = Vector3(rand_range(-10,10),rand_range(-10,10),rand_range(-10,10))
 		#print_debug("adding terrain",id,test_vec_delete)
 		ServerNetwork.get(client_id).create_terrain(id,test_vec_delete)
-		#ServerNetwork.get(client_id).get_all_terrain()
+		ServerNetwork.get(client_id).get_all_terrain()
