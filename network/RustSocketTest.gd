78f589b594354bf6e1d3a2ddf5723c6641196bb2
fearlesspandas <landonrenzullo@gmail.com>
2024-07-10 22:13:09 -0400

    double channel test

--- network/RustSocketTest.gd
+++ network/RustSocketTest.gd
@@ -6,15 +6,21 @@ extends Node
 # var b = "text"
 
 onready var rust_client = RustSocket.new()
+onready var rust_client2 = RustSocket.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.add_child(rust_client)
+	self.add_child(rust_client2)
 	rust_client._client.connect("data_received",self,"handle_data")
+	rust_client2._client.connect("data_received",self,"handle_data")
 	rust_client.connect_to_server()
+	rust_client2.connect_to_server()
 	
 func handle_data():
+	#for i in range(0 , 100):
 	var msg = rust_client.get_packet()
-	print_debug("Message received:", msg)
+	var msg2 = rust_client2.get_packet()
+	print_debug("Message received {client 1 : ", msg ,"}", "{client 2 : " + msg2 + "}")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
