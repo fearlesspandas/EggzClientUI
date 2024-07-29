d7c2debbef54d3120cf96e8bb1401a1a63ee95a0
fearlesspandas <landonrenzullo@gmail.com>
2024-07-29 17:01:39 -0400

    testing around perf issues

--- world/Chunk.gd
+++ world/Chunk.gd
@@ -10,23 +10,27 @@ var radius:float
 var entity_manager:EntityManagement
 onready var timer:Timer = Timer.new()
 var has_loaded = false
+var is_server = false
+var player
 func _ready():
-	timer.wait_time = 2
-	timer.connect("timeout",self,"_check_load")
+	timer.wait_time = 5
+	timer.connect("timeout",self,"check_load")
 	self.add_child(timer)
 	timer.start()
 	pass
-func load_terrain():	
-	ServerNetwork.get(client_id).get_cached_terrain(uuid)
-	has_loaded = true
+func load_terrain():
+	if !has_loaded:
+		ServerNetwork.get(client_id).get_cached_terrain(uuid)
+		has_loaded = true
 
-func _check_load():
-	var entities = entity_manager.client_entities.values() + entity_manager.server_entities.values()
+func check_load():
+	if !has_loaded:
+		var entities = entity_manager.client_entities.values() + entity_manager.server_entities.values()
 	#print_debug("null entities")
-	if entities != null:
+		if entities != null:
 		#print_debug("Non null entities")
-		for e in entities:
-			var loc:Vector3 = e.global_transform.origin
-			if !has_loaded and abs(loc.x - center.x) < radius and abs(loc.y - center.y) < radius and abs(loc.z - center.z) < radius:
-				print_debug("loading terrain")
-				load_terrain()
+			for e in entities:
+				var loc:Vector3 = e.global_transform.origin
+				if !has_loaded and abs(loc.x - center.x) < radius and abs(loc.y - center.y) < radius and abs(loc.z - center.z) < radius:
+					print_debug("loading terrain")
+					load_terrain()
