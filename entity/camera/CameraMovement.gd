9ac49118c4c05ee9eee71545efa0d6826fa44249
fearlesspandas <landonrenzullo@gmail.com>
2024-09-08 15:38:30 -0400

    made camera distance farther

--- entity/camera/CameraMovement.gd
+++ entity/camera/CameraMovement.gd
@@ -16,13 +16,15 @@ var sensitivity_x = 0.5
 var sensitivity_y = 25
 var rotating = false
 var maxzoom = 5
-var minzoom = 60
+var minzoom = 120
 var is_active:bool = false
+
 func check_mouse_position_within_viewport(position:Vector2) -> bool:
 	var viewport:Viewport = get_viewport()
 	var mins = viewport.global_canvas_transform.origin
 	var maxs = viewport.global_canvas_transform.origin + viewport.size
 	return mins.x < position.x and mins.y < position.y and maxs.x > position.x and maxs.y > position.y
+
 func _input(event):
 	if event is InputEventMouse and check_mouse_position_within_viewport(event.position) and is_active:
 		if event is InputEventMouseMotion and rotating:
@@ -30,7 +32,6 @@ func _input(event):
 			camera_root.rotation_degrees.x -= rot.x
 			#camera_root.rotation_degrees.x = clamp(camera_root.rotation_degrees.x,minx,max_x)
 			camera_root.global_rotate(Vector3.UP,-rot.y)
-			
 		if event is InputEventMouseButton and event.is_action("scroll_in"):
 			var diff = camera_root.global_transform.origin - camera_body.global_transform.origin
 			if diff.length() > maxzoom:
@@ -56,15 +57,12 @@ func _physics_process(delta):
 	camera_body.look_at(camera_root.global_transform.origin,Vector3.UP)
 	#print("trasnform:" + str(camera_root.global_transform))
 	pass
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	OS.window_fullscreen = false
-	
 	pass # Replace with function body.
+
 func set_active(active:bool):
 	is_active = active
 		
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
