2dab892e2955828c6f47a681441a868a4fc45dc3
fearlesspandas <landonrenzullo@gmail.com>
2025-03-16 00:27:29 -0400

    basic tie ins for socket status signals

--- native_lib/src/socket_stats.rs
+++ native_lib/src/socket_stats.rs
@@ -10,6 +10,54 @@ use tokio::sync::mpsc;
 type Sender<T> = mpsc::UnboundedSender<T>;
 type Receiver<T> = mpsc::UnboundedReceiver<T>;
 
+#[derive(NativeClass)]
+#[inherit(Control)]
+pub struct ConnectionIndicator{
+    bg_rect:Ref<ColorRect>,
+    main_rect:Ref<ColorRect>,
+    label:Ref<Label>,
+    tx:Sender<Action>,
+    hovering:bool,
+}
+impl InstancedDefault<Control,Sender<Action>> for ConnectionIndicator{
+    fn make(args:&Sender<Action>) -> Self{
+        ConnectionIndicator{
+            bg_rect:ColorRect::new().into_shared(),
+            main_rect:ColorRect::new().into_shared(),
+            label:Label::new().into_shared(),
+            tx:args.clone(),
+            hovering:false,
+            
+        }
+    }
+}
+impl Windowed<Action> for ConnectionIndicator{
+    const BG_HIGHLIGHT_COLOR:Color = Color{r:255.0,g:25.0,b:0.0,a:1.0};
+    const BG_COLOR:Color = Color{r:255.0,g:0.0,b:0.0,a:1.0};
+    const MAIN_COLOR:Color = Color{r:255.0,g:0.0,b:0.0,a:1.0};
+    const MARGIN_SIZE:f32 = 5.0;
+    fn hovering(&self) -> bool {self.hovering}
+    fn set_hovering(&mut self,value:bool){self.hovering = value}
+    fn centering(&self) -> Centering {Centering::center}
+    fn tx(&self) -> &Sender<Action> {&self.tx}
+    fn bg_rect(&self) -> &Ref<ColorRect>{&self.bg_rect}
+    fn main_rect(&self) -> &Ref<ColorRect>{&self.main_rect}
+}
+#[methods]
+impl ConnectionIndicator{
+    #[method]
+    fn _ready(&self,#[base] owner:TRef<Control>){
+        <Self as Windowed<Action>>::ready(self,owner);
+    }
+    #[method]
+    fn _process(&self,#[base] owner:TRef<Control>,delta:f64){
+        <Self as Windowed<Action>>::process(self,owner,delta);
+    }
+    #[method]
+    fn _input(&self,#[base] owner:TRef<Control>,event:Ref<InputEvent>){
+        <Self as Windowed<Action>>::input(self,owner,event.clone());
+    }
+}
 
 #[derive(NativeClass)]
 #[inherit(Control)]
@@ -17,7 +65,6 @@ pub struct ServerStats{
     bg_rect:Ref<ColorRect>,
     main_rect:Ref<ColorRect>,
     label:Ref<Label>,
-    desc_label:Instance<HoverStats>,
     tx:Sender<Action>,
     hovering:bool,
 }
@@ -27,7 +74,6 @@ impl InstancedDefault<Control,Sender<Action>> for ServerStats{
             bg_rect:ColorRect::new().into_shared(),
             main_rect:ColorRect::new().into_shared(),
             label:Label::new().into_shared(),
-            desc_label:HoverStats::make_instance().into_shared(),
             tx:args.clone(),
             hovering:false,
         }
@@ -61,10 +107,6 @@ impl ServerStats{
     #[method]
     fn _input(&self,#[base] owner:TRef<Control>,event:Ref<InputEvent>){
         <Self as Windowed<Action>>::input(self,owner,event.clone());
-    }
-    #[method]
-    fn set_amount(&self,amount:i32){
-        self.set_text(amount.to_string());
     }
 }
 #[derive(NativeClass)]
--- network/SharedRuntimeEnv.gd
+++ network/SharedRuntimeEnv.gd
@@ -5,6 +5,8 @@ onready var physics_native_shared_socket = load("res://native_lib/SharedRuntimeB
 
 func _ready():
 	ClientTerminalGlobalSignals.connect("request_data",self,"send_requested_data")
+	physics_native_shared_socket.connect("connected",self,"socket_connected")
+	physics_native_shared_socket.connect("disconnected",self,"socket_disconnected")
 
 func initialize_sockets():
 	var url = NetworkConfig.physics_host
@@ -17,4 +19,10 @@ func send_requested_data(data_type):
 			ClientTerminalGlobalSignals.add_graph_data("bytes_received_total_mb_" + str(OS.get_ticks_msec()) ,float(physics_native_shared_socket.num_all_bytes_received())/1000000.0)
 		_:
 			pass
+
+func socket_connected(socket_id,entities):
+	print_debug("Socket Connected ", socket_id, " for ", str(entities))
+
+func socket_disconnected(socket_id,entities):
+	assert(false)
 
